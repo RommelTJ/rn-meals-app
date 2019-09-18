ae9d22848782a8d2c4f19f29e2b0b552faf02562
Rommel Rico <me@rommelrico.com>
2019-09-18 07:06:37 -0700

    :sparkles: Screens: Adding dummy views and styles to all the screens

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
-const CategoryMealsScreen = (props) => {};
+const CategoryMealsScreen = (props) => {
+  return (
+    <View style={styles.screen}>
+      <Text>The category meal screen!</Text>
+    </View>
+  );
+};
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
 
 export default CategoryMealsScreen;
