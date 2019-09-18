45e1bf0701d28963568f52ea7bed7f5e60e813f0
Rommel Rico <me@rommelrico.com>
2019-09-18 07:03:25 -0700

    :sparkles: FiltersScreen: Initial commit

---
+++ screens/FiltersScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const FiltersScreen = (props) => {};
+
+const styles = StyleSheet.create({});
+
+export default FiltersScreen;
