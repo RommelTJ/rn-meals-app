f1e157fee5cb26212cc37f64be730f720d8d4f41
Rommel Rico <me@rommelrico.com>
2019-09-21 05:26:04 -0700

    :sparkles: CategoryMealsScreen: Navigating to MealDetailsScreen

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {View, Text, StyleSheet, Button} from 'react-native';
 
 const CategoryMealsScreen = (props) => {
   return (
     <View style={styles.screen}>
       <Text>The category meal screen!</Text>
+      <Button title="Go to Meal Details!" onPress={() => props.navigation.navigate({routeName: "MealDetail"})} />
     </View>
   );
 };
