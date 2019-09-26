f42edb42ced2f38c377aaa8b6d8f4807931732b3
Rommel Rico <me@rommelrico.com>
2019-09-26 07:02:32 -0700

    :sparkles: CategoryMealsScreen: Dynamic navigation options

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -3,17 +3,21 @@ import {View, Text, StyleSheet, Button} from 'react-native';
 import { CATEGORIES } from "../data/dummy-data";
 
 const CategoryMealsScreen = (props) => {
-  const catId = props.navigation.getParam("categoryId");
-  const selectedCategory = CATEGORIES.find(x => x.id === catId);
-
   return (
     <View style={styles.screen}>
       <Text>The category meal screen!</Text>
-      <Text>{selectedCategory.title}</Text>
       <Button title="Go to Meal Details!" onPress={() => props.navigation.navigate({routeName: "MealDetail"})} />
       <Button title="Go Back" onPress={() => props.navigation.pop()} />
     </View>
   );
+};
+
+CategoryMealsScreen.navigationOptions = (navigationData) => {
+  const catId = navigationData.navigation.getParam("categoryId");
+  const selectedCategory = CATEGORIES.find(x => x.id === catId);
+  return {
+    headerTitle: selectedCategory.title
+  }
 };
 
 const styles = StyleSheet.create({
