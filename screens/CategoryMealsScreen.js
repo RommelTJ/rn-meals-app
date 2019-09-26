1c60c2c55022fcb49bfd497665fde870df6e7945
Rommel Rico <me@rommelrico.com>
2019-09-26 06:57:57 -0700

    :sparkles: CategoryMeals: Showing the category title

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
+import { CATEGORIES } from "../data/dummy-data";
 
 const CategoryMealsScreen = (props) => {
   const catId = props.navigation.getParam("categoryId");
+  const selectedCategory = CATEGORIES.find(x => x.id === catId);
 
   return (
     <View style={styles.screen}>
       <Text>The category meal screen!</Text>
+      <Text>{selectedCategory.title}</Text>
       <Button title="Go to Meal Details!" onPress={() => props.navigation.navigate({routeName: "MealDetail"})} />
       <Button title="Go Back" onPress={() => props.navigation.pop()} />
     </View>
