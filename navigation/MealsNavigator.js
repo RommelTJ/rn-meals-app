ad6b08226b8f56cc2e956ded034ece03b047389e
Rommel Rico <me@rommelrico.com>
2019-09-27 07:07:45 -0700

    :sparkles: MealsNavigator: Setting up default navigation options

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -4,11 +4,21 @@ import { createStackNavigator } from "react-navigation-stack";
 import CategoriesScreen from "../screens/CategoriesScreen";
 import CategoryMealsScreen from "../screens/CategoryMealsScreen";
 import MealDetailScreen from "../screens/MealDetailScreen";
+import {Platform} from "react-native";
+import Colors from "../constants/Colors";
 
 
 const MealsNavigator = createStackNavigator({
   Categories: CategoriesScreen,
-  CategoryMeals: CategoryMealsScreen,
+  CategoryMeals: {
+    screen: CategoryMealsScreen,
+    navigationOptions: {
+      headerStyle: {
+        backgroundColor: Platform.OS === "android" ? Colors.primaryColor : ""
+      },
+      headerTintColor: Platform.OS === "android" ? "white" : Colors.primaryColor
+    }
+  },
   MealDetail: MealDetailScreen
 });
 
