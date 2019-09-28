75b3854cfee26aaab021b99fbeb107b1dba42a44
Rommel Rico <me@rommelrico.com>
2019-09-28 08:10:47 -0700

    :sparkles: CategoryGridTile: Loading TouchableNativeFeedback if android

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import {TouchableOpacity, View, Text, StyleSheet} from "react-native";
+import {TouchableOpacity, View, Text, StyleSheet, TouchableNativeFeedback, Platform} from "react-native";
+
 
 const CategoryGridTile = (props) => {
+  let TouchableComponent = TouchableOpacity;
+  if (Platform.OS === "android" && Platform.Version >= 21) {
+    TouchableComponent = TouchableNativeFeedback
+  }
+
   return (
-    <TouchableOpacity style={styles.gridItem} onPress={() => props.onSelect()}>
+    <TouchableComponent style={styles.gridItem} onPress={() => props.onSelect()}>
       <View style={{...styles.container, ...{backgroundColor: props.color}}}>
         <Text style={styles.title} numberOfLines={2} >{props.title}</Text>
       </View>
-    </TouchableOpacity>
+    </TouchableComponent>
   );
 };
 
