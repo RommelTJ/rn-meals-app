2ec6318a2f18e81669ad51ddb55c5698e487611b
Rommel Rico <me@rommelrico.com>
2019-09-28 08:03:48 -0700

    :lipstick: CategoryGridTile: More styling

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -18,7 +18,16 @@ const styles = StyleSheet.create({
     height: 150
   },
   container: {
-    flex: 1
+    flex: 1,
+    borderRadius: 10,
+    shadowColor: "black",
+    shadowOpacity: 0.26,
+    shadowOffset: {width: 0, height: 2},
+    shadowRadius: 10,
+    elevation: 3,
+    padding: 15,
+    justifyContent: "flex-end",
+    alignItems: "flex-end"
   }
 });
 
