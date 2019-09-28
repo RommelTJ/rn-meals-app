cda7a5beb76a2568d52f346520f707f7d2ecd0b1
Rommel Rico <me@rommelrico.com>
2019-09-28 08:08:03 -0700

    :sparkles: CategoryGridTile: Setting number of lines and alignment

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -5,7 +5,7 @@ const CategoryGridTile = (props) => {
   return (
     <TouchableOpacity style={styles.gridItem} onPress={() => props.onSelect()}>
       <View style={{...styles.container, ...{backgroundColor: props.color}}}>
-        <Text style={styles.title}>{props.title}</Text>
+        <Text style={styles.title} numberOfLines={2} >{props.title}</Text>
       </View>
     </TouchableOpacity>
   );
@@ -31,7 +31,8 @@ const styles = StyleSheet.create({
   },
   title: {
     fontFamily: "open-sans-bold",
-    fontSize: 22
+    fontSize: 22,
+    textAlign: "right"
   }
 });
 
