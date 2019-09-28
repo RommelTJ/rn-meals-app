4f78713f37589af5aaf547d453393174489aa1f6
Rommel Rico <me@rommelrico.com>
2019-09-28 07:59:59 -0700

    :lipstick: CategoryGridTile: Making the color bigger

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -4,7 +4,7 @@ import {TouchableOpacity, View, Text, StyleSheet} from "react-native";
 const CategoryGridTile = (props) => {
   return (
     <TouchableOpacity style={styles.gridItem} onPress={() => props.onSelect()}>
-      <View style={{backgroundColor: props.color}}>
+      <View style={{...styles.container, ...{backgroundColor: props.color}}}>
         <Text>{props.title}</Text>
       </View>
     </TouchableOpacity>
@@ -16,6 +16,9 @@ const styles = StyleSheet.create({
     flex: 1,
     margin: 15,
     height: 150
+  },
+  container: {
+    flex: 1
   }
 });
 
