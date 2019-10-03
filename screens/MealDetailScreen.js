f64fba2ef0b45e95a336d79b049b2511ede81ba4
Rommel Rico <me@rommelrico.com>
2019-10-02 20:43:50 -0700

    :sparkles: MealDetailScreen: Using the HeaderButton

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {View, Text, StyleSheet, Button} from 'react-native';
 import {MEALS} from "../data/dummy-data";
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import CustomHeaderButton from "../components/HeaderButton";
 
 const MealDetailScreen = (props) => {
   const mealId = props.navigation.getParam("mealId");
@@ -19,7 +21,12 @@ MealDetailScreen.navigationOptions = (navigationData) => {
   const selectedMeal = MEALS.find(meal => meal.id === mealId);
 
   return {
-    headerTitle: selectedMeal.title
+    headerTitle: selectedMeal.title,
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+        <Item title='Favorite' iconName='ios-star' onPress={() => {console.log('mark as favorite!')}} />
+      </HeaderButtons>
+    )
   };
 };
 
