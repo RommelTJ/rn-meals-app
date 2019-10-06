3c72c43aa3b31dc9c9a6460fa5117430ebaa17a3
Rommel Rico <me@rommelrico.com>
2019-10-06 10:24:39 -0700

    :sparkles: CategoriesScreen: Showing the menu icon

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { FlatList, StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { FlatList, StyleSheet } from 'react-native';
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 
 import { CATEGORIES } from "../data/dummy-data";
 import CategoryGridTile from "../components/CategoryGridTile";
+import CustomHeaderButton from "../components/HeaderButton";
 
 
 // Note: Any component mapped to identifiers in navigators gets the "navigation" props
@@ -37,7 +39,12 @@ const CategoriesScreen = (props) => {
 };
 
 CategoriesScreen.navigationOptions = {
-  headerTitle: "Meal Categories"
+  headerTitle: "Meal Categories",
+  headerLeft: (
+    <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+      <Item title="Menu" iconName="ios-menu" onPress={() => {}} />
+    </HeaderButtons>
+  )
 };
 
 const styles = StyleSheet.create({
