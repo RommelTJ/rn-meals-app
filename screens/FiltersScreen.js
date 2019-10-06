91e7f8b75712b94d1016727a9142a17b54b52108
Rommel Rico <me@rommelrico.com>
2019-10-06 10:30:53 -0700

    :sparkles: FiltersScreen: Adding a menu icon and navigation options

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import {HeaderButtons, Item} from "react-navigation-header-buttons";
+import CustomHeaderButton from "../components/HeaderButton";
 
 const FiltersScreen = (props) => {
   return (
@@ -9,8 +11,21 @@ const FiltersScreen = (props) => {
   );
 };
 
-FiltersScreen.navigationOptions = {
-  headerTitle: "Filter Meals"
+FiltersScreen.navigationOptions = (navData) => {
+  return (
+    {
+      headerTitle: "Filter Meals",
+      headerLeft: (
+        <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+          <Item
+            title="Menu"
+            iconName="ios-menu"
+            onPress={() => navData.navigation.toggleDrawer()}
+          />
+        </HeaderButtons>
+      )
+    }
+  );
 };
 
 const styles = StyleSheet.create({
