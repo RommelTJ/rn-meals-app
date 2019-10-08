f5936a42fcbe5d115102f3192f5799c0f8e55a2c
Rommel Rico <me@rommelrico.com>
2019-10-07 19:16:53 -0700

    :sparkles: MealDetailScreen: Adding scaffolding for the content

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,18 +1,27 @@
 import React from 'react';
-import {View, Text, StyleSheet, Button} from 'react-native';
-import {MEALS} from "../data/dummy-data";
+import { ScrollView, Image, View, Text, StyleSheet, Button } from 'react-native';
+import { MEALS } from "../data/dummy-data";
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import CustomHeaderButton from "../components/HeaderButton";
+import DefaultText from "../components/DefaultText";
 
 const MealDetailScreen = (props) => {
   const mealId = props.navigation.getParam("mealId");
   const selectedMeal = MEALS.find(meal => meal.id === mealId);
 
   return (
-    <View style={styles.screen}>
-      <Text>{selectedMeal.title}</Text>
-      <Button title="Go Back To Categories" onPress={() => props.navigation.popToTop()} />
-    </View>
+    <ScrollView>
+      <Image />
+      <View style={{...styles.mealRow, ...styles.mealDetail}}>
+        <DefaultText>{props.duration}m</DefaultText>
+        <DefaultText>{props.complexity.toUpperCase()}</DefaultText>
+        <DefaultText>{props.affordability.toUpperCase()}</DefaultText>
+      </View>
+      <Text style={styles.title}>Ingredients</Text>
+      <Text>List of ingredients...</Text>
+      <Text style={styles.title}>Steps</Text>
+      <Text>List of steps...</Text>
+    </ScrollView>
   );
 };
 
