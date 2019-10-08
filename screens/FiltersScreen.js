7fa48e425655a6d6d9dcda4ef7513e17b7bf00a7
Rommel Rico <me@rommelrico.com>
2019-10-08 06:53:12 -0700

    :sparkles: FiltersScreen: Adding structure for filters

--- screens/FiltersScreen.js
+++ screens/FiltersScreen.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import {HeaderButtons, Item} from "react-navigation-header-buttons";
+import { View, Text, StyleSheet, Switch } from 'react-native';
+import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import CustomHeaderButton from "../components/HeaderButton";
 
 const FiltersScreen = (props) => {
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Available Filters / Restrictions</Text>
+      <View style={styles.filterContainer}>
+        <Text>Gluten-free</Text>
+        <Switch />
+      </View>
     </View>
   );
 };
@@ -33,7 +37,9 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
-  }
+  },
+  title: {},
+  filterContainer: {}
 });
 
 export default FiltersScreen;
