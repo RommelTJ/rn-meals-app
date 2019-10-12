094d87b5f04ff17f0bf1f83d08f191a499ad868e
Rommel Rico <me@rommelrico.com>
2019-10-11 19:16:58 -0700

    :sparkles: MealDetailScreen: Passing redux data to navigation options

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { useSelector } from "react-redux";
 import { ScrollView, Image, View, Text, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
@@ -18,6 +18,10 @@ const MealDetailScreen = (props) => {
   const availableMeals = useSelector(state => state.meals.meals);
   const mealId = props.navigation.getParam("mealId");
   const selectedMeal = availableMeals.find(meal => meal.id === mealId);
+
+  useEffect(() => {
+    props.navigation.setParams({mealTitle: selectedMeal.title});
+  }, [selectedMeal]);
 
   return (
     <ScrollView>
@@ -44,11 +48,10 @@ const MealDetailScreen = (props) => {
 };
 
 MealDetailScreen.navigationOptions = (navigationData) => {
-  const mealId = navigationData.navigation.getParam("mealId");
-  const selectedMeal = MEALS.find(meal => meal.id === mealId);
+  const mealTitle = navigationData.navigation.getParam("mealTitle");
 
   return {
-    headerTitle: selectedMeal.title,
+    headerTitle: mealTitle,
     headerRight: (
       <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
         <Item title='Favorite' iconName='ios-star' onPress={() => {console.log('mark as favorite!')}} />
