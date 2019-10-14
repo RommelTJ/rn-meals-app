f62fb69a718dc3fa5724bfd1c5e31d9a60571005
Rommel Rico <me@rommelrico.com>
2019-10-14 07:23:06 -0700

    :sparkles: CategoryMealScreen: Displaying a message if everything is filtered out

--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import { View, StyleSheet } from 'react-native';
 import { useSelector } from "react-redux";
 import { CATEGORIES } from "../data/dummy-data";
 import MealList from "../components/MealList";
+import DefaultText from "../components/DefaultText";
 
 const CategoryMealsScreen = (props) => {
   const catId = props.navigation.getParam("categoryId");
@@ -9,6 +11,14 @@ const CategoryMealsScreen = (props) => {
   const displayedMeals = availableMeals.filter(
     meal => meal.categoryIds.indexOf(catId) >= 0
   );
+
+  if (displayedMeals.length === 0) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No meals found. Maybe check your filters.</DefaultText>
+      </View>
+    );
+  }
 
   return <MealList listData={displayedMeals} navigation={props.navigation} />;
 };
@@ -20,5 +30,13 @@ CategoryMealsScreen.navigationOptions = (navigationData) => {
     headerTitle: selectedCategory.title
   }
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
 
 export default CategoryMealsScreen;
